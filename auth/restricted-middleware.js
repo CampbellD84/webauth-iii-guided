41d2143bd5b7cd20017deeaf5b60f597d995e57c
Douglas Campbell <dacampbelltbone@gmail.com>
2019-08-26 23:55:18 -0400

    Completed guided project

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,24 +1,25 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const secrets = require("../config/secrets");
 
-const Users = require('../users/users-model.js');
+const Users = require("../users/users-model.js");
 
 module.exports = (req, res, next) => {
-  const { username, password } = req.headers;
+  const token = req.headers.authorization;
 
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: 'Invalid Credentials' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json({ message: 'Ran into an unexpected error' });
-      });
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (err, decodedToken) => {
+      if (err) {
+        // bad token
+        res.status(401).json({ message: "wutdahek?" });
+      } else {
+        //decodedToken
+        req.decodedJwt = decodedToken;
+        next();
+        // yay us
+      }
+    });
   } else {
-    res.status(400).json({ message: 'No credentials provided' });
+    res.status(401).json({ message: "You do not pass" });
   }
 };
